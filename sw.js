0beccc6316a1b8b0ad15da0bee4dfef123e5776c
Muh Rusdi <muhamamdrusdi@icloud.com>
2018-03-16 10:57:10 +0800

    add google analytics

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-085d822b37b3b1379818.js","2cc87ccf27c1fdea3ced58fa8d5851fd"],["/app-31f68ebbd13c04f99abb.js","48bf0111e60534e419b699281561db81"],["/app-7801e6e68794ab99aa30.js","0675c95f9b9ec441bc47bdf0064a8de8"],["/app-93090cbc9806a83b787a.js","c2bbb4ce620e11e24fd9d3ba47a88c95"],["/app-a81e3daa6ed45caae907.js","78c8a7ebbb5d411f30b27e6d412eac19"],["/app-b5d5fb4e21052754f433.js","6099961e031f2dad5a8d7041ac95e237"],["/commons-1889de443e268ebe49b5.js","a8ff11daa1d5aeacfcfbdd6502e91a83"],["/commons-537028d01e02b0b0cc22.js","4a877d73f57bcac7a790fbf262618511"],["/index.html","462c759a290febb73c201665861faf53"],["/offline-plugin-app-shell-fallback/index.html","068beb5e730f30c0579a38bb03f36a42"]];
+var precacheConfig = [["/app-085d822b37b3b1379818.js","dc9a8cd1276b935ccf91649020b9585b"],["/app-31f68ebbd13c04f99abb.js","48bf0111e60534e419b699281561db81"],["/app-623d487b7472c1f119c4.js","98ede060b82d58488d5af8334094e92c"],["/app-7801e6e68794ab99aa30.js","0675c95f9b9ec441bc47bdf0064a8de8"],["/app-93090cbc9806a83b787a.js","c2bbb4ce620e11e24fd9d3ba47a88c95"],["/app-a81e3daa6ed45caae907.js","78c8a7ebbb5d411f30b27e6d412eac19"],["/app-b5d5fb4e21052754f433.js","6099961e031f2dad5a8d7041ac95e237"],["/commons-1889de443e268ebe49b5.js","a8ff11daa1d5aeacfcfbdd6502e91a83"],["/commons-537028d01e02b0b0cc22.js","9cc76a1aa0142bf1eff2828d338e4cb1"],["/index.html","6002a3268b788f8e9f6fc555349f59a4"],["/offline-plugin-app-shell-fallback/index.html","3db1f1ab02f38307834e45a744550e2d"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
